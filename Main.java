43c3244d6432d3be5bb80c36d4f41b6c46d740a5
JasonMillerPHS <jason.miller@patelhs.org>
2020-09-25 16:43:16 +0000

    I added three ints, containing the information needed for it to calculate if it was my birthday or not. I also added if and else statements so when it was my birthday it added one to my age and said I am (blank) years old. As well as the else saying I am still (blank) years old. I was unable to change Main.java to CheckMyAge as it did not let me change the file name.

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main  { // It does not let me change the file name to CheckMyAge
   public static void main(String[] args) {
   
     int myAge = 16;
-    int myBirthday = 901; // input date as 901 or 1021 putting a 0 infront results in an error
+    int myBirthday = 901; // input date as 901 or 1021 putting a 0 infront results in an error.
     int todaysDate = 923;
 
     if (todaysDate == myBirthday) {
